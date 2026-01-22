de0b6d5fe248b42ef786d6e91615945a3ef66966
Max-sm-yc <sunm@countryday.net>
2026-01-22 13:52:39 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+import requests
 from st_supabase_connection import SupabaseConnection, execute_query
 
 # --- PAGE CONFIG ---
